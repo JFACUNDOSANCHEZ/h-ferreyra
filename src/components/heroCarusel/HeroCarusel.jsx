f7198d129db2aff88cc6f486b78568258ab71cee
Facu Sanchez <facusanchz96@gmail.com>
2025-05-20 22:28:39 -0300

    carusel

--- src/components/heroCarusel/HeroCarusel.jsx
+++ src/components/heroCarusel/HeroCarusel.jsx
@@ -8,21 +8,21 @@ import styles from './heroCarusel.module.css';
 
 const slides = [
   {
-    image: 'https://www.maimonides.edu/wp-content/uploads/2020/07/20200713-diagnostico-por-imagenes.jpg',
+    // image: 'https://www.maimonides.edu/wp-content/uploads/2020/07/20200713-diagnostico-por-imagenes.jpg',
     title: 'SOLUCIONES DE VOZ EN LA NUBE',
     description: 'Softswitch VSC y Centrex IP Multiempresa.',
     button: 'Saber más'
   }
   ,
   {
-    image: 'https://images.pexels.com/photos/13176358/pexels-photo-13176358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    // image: 'https://images.pexels.com/photos/13176358/pexels-photo-13176358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     title: 'AUTONOMÍA QUE SOLO EL LÍDER PUEDE ENTREGAR',
     description: 'Más de 450 ISPs utilizan nuestros servicios.',
     button: 'Comprar ahora'
   }
   ,
   // {
-  //   image: 'https://images.pexels.com/photos/7089017/pexels-photo-7089017.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  // //   image: 'https://images.pexels.com/photos/7089017/pexels-photo-7089017.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
   //   title: 'PLATAFORMA INNOVADORA PARA COMUNICACIÓN',
   //   description: 'Conecte su negocio con el mundo.',
   //   button: 'Ver detalles'
