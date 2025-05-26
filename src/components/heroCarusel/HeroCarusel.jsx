fac535c8218befc2b3f2e3fdedd50191c78400db
Facu Sanchez <facusanchz96@gmail.com>
2025-05-26 13:26:01 -0300

    navv

--- src/components/heroCarusel/HeroCarusel.jsx
+++ src/components/heroCarusel/HeroCarusel.jsx
@@ -9,24 +9,24 @@ import styles from './heroCarusel.module.css';
 const slides = [
   {
      image: 'https://www.maimonides.edu/wp-content/uploads/2020/07/20200713-diagnostico-por-imagenes.jpg',
-    title: 'SOLUCIONES DE VOZ EN LA NUBE',
-    description: 'Softswitch VSC y Centrex IP Multiempresa.',
+    title: 'HIS, RIS, PACs, EN LA NUVE',
+    description: ' Líder en diagnóstico por imagen.',
     button: 'Saber más'
   }
   ,
   {
     image: 'https://images.pexels.com/photos/13176358/pexels-photo-13176358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    title: 'AUTONOMÍA QUE SOLO EL LÍDER PUEDE ENTREGAR',
+    title: 'SEA PARTE DE NUESTRA COMUNIDAD TRSCLOUD',
     description: 'Más de 450 ISPs utilizan nuestros servicios.',
     button: 'Comprar ahora'
   }
   ,
-  // {
-  // //   image: 'https://images.pexels.com/photos/7089017/pexels-photo-7089017.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  //   title: 'PLATAFORMA INNOVADORA PARA COMUNICACIÓN',
-  //   description: 'Conecte su negocio con el mundo.',
-  //   button: 'Ver detalles'
-  // }
+  {
+     image: 'https://images.pexels.com/photos/7089017/pexels-photo-7089017.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    title: 'SOPORTE 24HS , INPLEMENTACION, SOFTWARE DEMO',
+    description: 'Conecte su negocio con el mundo.',
+    button: 'Ver detalles'
+  }
 ];
 
 export default function HeroCarousel() {
