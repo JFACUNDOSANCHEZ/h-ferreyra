b6f71b69de50cf3a0c00900f071af094c0e10ba6
JFACUNDOSANCHEZ <facusanchz96@gmail.com>
2025-06-24 00:29:41 -0300

    prto

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
 // src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from '../src/components/home/Home'
-import Producto from '../src/components/producto/Producto'
-import NavBar from './components/Nav/NavBar.jsx'
-import Footer from '../src/components/footer/Footer'
-import WhatsappButton from '../src/components/whatsapp/WhatsappButton.jsx'
-import AboutUs from './components/aboutUs/AboutUs'
-import Contact from './components/contact/Contact'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './components/home/Home';
+import Producto from './components/producto/Producto';
+import NavBar from './components/Nav/NavBar';
+import Footer from './components/footer/Footer';
+import WhatsappButton from './components/whatsapp/WhatsappButton';
+import AboutUs from './components/aboutUs/AboutUs';
+import Contact from './components/contact/Contact';
+
 function App() {
   return (
     <Router>
       <NavBar />
+
       <Routes>
-        <Route path="/#" element={<Home />} />
-       <Route path="/#vmcloud" element={<Producto />} /> *
-    <Route path='/#nosotros' element={<AboutUs/>} />
-      <Route path='/#contacto' element={<Contact></Contact>}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/vmcloud" element={<Producto />} />
+        <Route path="/nosotros" element={<AboutUs />} />
+        <Route path="/contacto" element={<Contact />} />
       </Routes>
-      <Footer></Footer>
-         <WhatsappButton />
+
+      <Footer />
+      <WhatsappButton />
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
 
